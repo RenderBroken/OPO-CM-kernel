3d498f4d29ca7b8a7a61dce163fa8ad671a6b779
savoca <adeddo27@gmail.com>
2014-07-10 23:59:35 +0000

    Voltage Control: Fix voltage setting bug

--- arch/arm/mach-msm/clock-krait-8974.c
+++ arch/arm/mach-msm/clock-krait-8974.c
@@ -630,6 +630,8 @@ module_param(pvs_config_ver, uint, S_IRUGO);
 
 extern bool is_used_by_scaling(unsigned int freq);
 
+static unsigned int cnt;
+
 ssize_t show_UV_mV_table(struct cpufreq_policy *policy, char *buf)
 {
 	int i, freq, len = 0;
@@ -665,6 +667,11 @@ ssize_t store_UV_mV_table(struct cpufreq_policy *policy, char *buf,
 	/* use only master core 0 */
 	int num_levels = cpu_clk[0]->vdd_class->num_levels;
 
+	if (cnt) {
+		cnt = 0;
+		return -EINVAL;
+	}
+
 	/* sanity checks */
 	if (num_levels < 0)
 		return -1;
@@ -687,7 +694,8 @@ ssize_t store_UV_mV_table(struct cpufreq_policy *policy, char *buf,
 
 		/* Non-standard sysfs interface: advance buf */
 		ret = sscanf(buf, "%s", size_cur);
-		buf += strlen(size_cur) + 1;
+		cnt = strlen(size_cur);
+		buf += cnt + 1;
 	}
 	pr_warn("faux123: user voltage table modified!\n");
 
