f557768a9df80159b98118c4c94898371ee66b77
anarkia1976 <stefano.villa1976@gmail.com>
2014-07-28 23:06:08 +0200

    cpufreq: cpu-boost: enable at boot

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -43,7 +43,7 @@ static struct workqueue_struct *cpu_boost_wq;
 
 static struct work_struct input_boost_work;
 
-static unsigned int cpu_boost = 0;
+static unsigned int cpu_boost = 1;
 module_param(cpu_boost, uint, 0644);
 
 static unsigned int boost_ms;
