efebd40e4dac098189efa4e59ca5c676e1e8d42a
anarkia1976 <stefano.villa1976@gmail.com>
2014-11-12 13:08:06 +0100

    fastcharge: limit the max charge level to 2100 mA
    Conflicts:
    	arch/arm/mach-msm/Makefile

--- arch/arm/mach-msm/fastchg.c
+++ arch/arm/mach-msm/fastchg.c
@@ -81,7 +81,7 @@ static ssize_t charge_level_store(struct kobject *kobj,
 		case FAST_CHARGE_1200:
 		case FAST_CHARGE_1500:
 		case FAST_CHARGE_2000:
-		case FAST_CHARGE_2200:
+		case FAST_CHARGE_2100:
 			fast_charge_level = new_charge_level;
 			return count;
 		default:
