af11e1b65cce6ddbc0c2953cbc48303afa980f3f
anarkia1976 <stefano.villa1976@gmail.com>
2015-04-24 14:03:55 +0200

    drivers: notifier: switch to backlight solution
    Conflicts:
    	drivers/video/backlight/lm3630_bl.c
    
    Conflicts:
    	drivers/video/backlight/lm3630_bl.c
    	drivers/video/msm/mdss/mdss_dsi.c

--- drivers/video/backlight/lm3630_bl.c
+++ drivers/video/backlight/lm3630_bl.c
@@ -31,6 +31,9 @@
 #ifdef CONFIG_MACH_OPPO
 #include <linux/boot_mode.h>
 #endif //CONFIG_MACH_OPPO
+#ifdef CONFIG_STATE_NOTIFIER
+#include <linux/state_notifier.h>
+#endif
 #define REG_CTRL	0x00
 #define REG_CONFIG	0x01
 #define REG_BRT_A	0x03
@@ -201,6 +204,17 @@ static int lm3630_intr_config(struct lm3630_chip_data *pchip)
 	int ret;
 	struct lm3630_chip_data *pchip = lm3630_pchip;
 	pr_debug("%s: bl=%d\n", __func__,bl_level);
+
+#ifdef CONFIG_STATE_NOTIFIER
+	// if display is switched off
+	if (!use_fb_notifier && bl_level == 0)
+		state_notifier_call_chain(STATE_NOTIFIER_SUSPEND, NULL);
+
+	// if display is switched on
+	if (!use_fb_notifier && bl_level != 0 && pre_brightness == 0)
+		state_notifier_call_chain(STATE_NOTIFIER_ACTIVE, NULL);
+#endif
+
 #ifdef CONFIG_MACH_OPPO
 /* Xiaori.Yuan@Mobile Phone Software Dept.Driver, 2014/04/28  Add for add log for 14001 black screen */
 		if(pre_brightness == 0)
