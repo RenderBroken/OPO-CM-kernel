bf7ece761ceb2778ea9ea2e252b9d8910993919a
anarkia1976 <stefano.villa1976@gmail.com>
2015-05-24 07:47:25 +0200

    cpufreq: cpu-boost: Respect max frequency
    * Don't step on thermald's toes

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -169,7 +169,11 @@ static int boost_adjust_notify(struct notifier_block *nb, unsigned long val,
 		return NOTIFY_OK;
 
 	min = max(b_min, ib_min);
-	min = min(min, policy->max);
+	if (min > policy->max) {
+		min = policy->max;
+		pr_debug("CPU%u boost min changed to conform to"
+					"max freq: %u kHz\n", cpu, min);
+	}
 
 	pr_debug("CPU%u policy min before boost: %u kHz\n",
 		 cpu, policy->min);
