bab8e5d3322a34031528c878692aec34afbae332
Zachariah <zkennedy87@gmail.com>
2015-06-06 11:34:32 -0400

    TESTING!!! Revert "tracing/sched: Track per-cpu rt and non-rt cpu_load."
    This reverts commit 4dc47fb113fee708933e503f96c3c6345678088d.
    
    Signed-off-by: Zachariah <zkennedy87@gmail.com>
    
    Conflicts:
    	kernel/sched/core.c

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -760,7 +760,6 @@ static void enqueue_task(struct rq *rq, struct task_struct *p, int flags)
 	sched_info_queued(p);
 	p->sched_class->enqueue_task(rq, p, flags);
 	inc_cumulative_runnable_avg(rq, p);
-	trace_sched_enq_deq_task(p, 1);
 }
 
 static void dequeue_task(struct rq *rq, struct task_struct *p, int flags)
@@ -769,7 +768,6 @@ static void dequeue_task(struct rq *rq, struct task_struct *p, int flags)
 	sched_info_dequeued(p);
 	p->sched_class->dequeue_task(rq, p, flags);
 	dec_cumulative_runnable_avg(rq, p);
-	trace_sched_enq_deq_task(p, 0);
 }
 
 void activate_task(struct rq *rq, struct task_struct *p, int flags)
